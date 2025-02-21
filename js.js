0e6129bd58c6948e118acad0f373ba8f0838000c
xNomada <jhon.michael.OF@proton.me>
2025-02-21 18:27:58 -0500

    Add playGame()

--- js.js
+++ js.js
@@ -10,25 +10,37 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-  const humanChoice = prompt('Choose between rock, paper and scissors').toLowerCase();
+  let humanChoice = prompt('Choose between rock, paper and scissors').toLowerCase();
   return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice){
-  if(humanChoice === "rock" && computerChoice === "scissors"){
-    humanScore += 1;
-  }else if(humanChoice === "paper" && computerChoice === "rock"){
-    humanScore += 1;
-  }else if(humanChoice === "scissors" && computerChoice === "paper"){
-    humanScore += 1;
-  }else {
-    computerScore += 1;
+function playGame(){
+
+  function playRound(humanChoice, computerChoice){
+    if(humanChoice === "rock" && computerChoice === "scissors"){
+      humanScore += 1;
+    }else if(humanChoice === "paper" && computerChoice === "rock"){
+      humanScore += 1;
+    }else if(humanChoice === "scissors" && computerChoice === "paper"){
+      humanScore += 1;
+    }else if(humanChoice === computerChoice){
+      console.log("Tie");
+    }else {
+      computerScore += 1;
+    }
   }
+
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  playRound(humanSelection, computerSelection);
+
+  console.log(computerSelection);
+  console.log("Human Score: " + humanScore);
+  console.log("Computer Score: " + computerScore);
 }
 
-playRound(getHumanChoice(), getComputerChoice());
 
-console.log(getComputerChoice());
-// console.log(getHumanChoice());
-console.log("Human Score: " + humanScore);
-console.log("Computer Score: " + computerScore);
+for(let i = 0; i< 5; i++){
+  playGame();
+}
