f71be3a7ed487beb278e23a5f5a32c3c02521f1e
xNomada <jhon.michael.OF@proton.me>
2025-02-21 18:03:15 -0500

    Add playRound function

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
   const randomChoice = Math.floor(Math.random() * 10);
   
@@ -7,9 +10,25 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-  const humanChoice = prompt('Choose between rock, paper and scissor').toLowerCase();
+  const humanChoice = prompt('Choose between rock, paper and scissors').toLowerCase();
   return humanChoice;
 }
 
+function playRound(humanChoice, computerChoice){
+  if(humanChoice === "rock" && computerChoice === "scissors"){
+    humanScore += 1;
+  }else if(humanChoice === "paper" && computerChoice === "rock"){
+    humanScore += 1;
+  }else if(humanChoice === "scissors" && computerChoice === "paper"){
+    humanScore += 1;
+  }else {
+    computerScore += 1;
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+
 console.log(getComputerChoice());
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: " + computerScore);
